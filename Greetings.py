c5085bc9d95105c4c3fa281705ea6aa68717574e
spaceship17 <112682678+spaceship17@users.noreply.github.com>
2023-02-19 20:29:23 +0530

    Add files via upload

---
+++ Greetings.py
@@ -0,0 +1,5 @@
+friends = ['abhishek', 'amisha', 'aman', 'bala', 'rohit', 'mayank', 'kajal']
+
+for friend in friends:
+    message = f"{friend.title()}, Please bless us for our wedding."
+    print(message)
