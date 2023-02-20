d118e5a933c728cba8a2d71a571fd6802c37e4cb
spaceship17 <112682678+spaceship17@users.noreply.github.com>
2023-02-20 12:01:29 +0530

    Add files via upload

---
+++ my_fav.py
@@ -0,0 +1,5 @@
+my_fav_wheels = ['Porsche', 'Mercedes', 'landrover', 'audi', 'Mini Cooper', 'ktm1200CC', 'ducati' ]
+
+for wheels in my_fav_wheels:
+    own = f"I would love to own a {wheels.upper()}"
+    print(own)
---
+++ organize_list.py
@@ -0,0 +1,27 @@
+#list of car i love
+cars = ['Porsche', 'Mercedes', 'landrover', 'audi', 'Mini Cooper', 'volvo']
+print(cars)
+
+#Original list
+print("\n\tHere is the original list: ")
+print(cars)
+
+#sort for once
+print("\n\tHere is the Sorted list: ")
+print(sorted(cars))
+
+#Original list again
+print("\n\tHere is the original list again: ")
+print(cars)
+
+#Sort my cars
+print("\n\tHere is the Permanent sorted list: ")
+cars.sort()
+print(cars)
+
+#reverse sort my cars
+print("\n\tHere is the Permanent reverse sorted list: ")
+cars.sort(reverse=True)
+print(cars)
+
+
